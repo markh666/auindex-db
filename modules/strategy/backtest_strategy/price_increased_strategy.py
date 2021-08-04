96703118c20a097ac3364b2cdadd3ea94aa1e7dd
mark <hanyu929@gmail.com>
2021-08-04 21:25:49 +1000

    update strategy

--- modules/strategy/backtest_strategy/price_increased_strategy.py
+++ modules/strategy/backtest_strategy/price_increased_strategy.py
@@ -1,10 +1,79 @@
 from .backtest_strategy import Backtest_Strategy
 from myutils import *
+from modules.test import t_test_statistic
+
 
 class Price_Increased_Strategy(Backtest_Strategy):
-    # to do
-    pass
+    def __init__(self, detect_strategy, db=None, symbol_list=None, days_interval=120, hold_priod=200, date_from="2019-02-05", date_to="2021-02-10", increased_threshold=2.0):
+        self.db = db
+        if symbol_list == None:
+            symbol_list = self.db.get_watchlist()
+        super(Price_Increased_Strategy, self).__init__(
+            detect_strategy=detect_strategy, symbol_list=symbol_list)
+        self.days_interval = days_interval
+        self.hold_priod = hold_priod
+        self.date_from = date_from
+        self.date_to = date_to
+        self.increased_threshold = increased_threshold
+
+    def _get_idx_of_date(self, dates, curr_date):
+        for i in range(len(dates)):
+            date = date_str_to_datetime(dates[i][0], include="ymd")
+            if date <= curr_date:
+                return i
+        return len(dates)-1
+
+    def price_explosed(self, buy_in_price, prices):
+        for price in prices:
+            if price[0]/buy_in_price >= self.increased_threshold:
+                print("buy in price "+str(buy_in_price))
+                print("explosed price "+str(price[0]))
+                return True
+        return False
+
+    def eval(self):
+        repeats = 0
+        success = 0
+        for symbol in self.symbol_list:
+            print("====> back testing "+symbol[0])
+            response = self.db.get_end_of_day_data(
+                symbol=symbol[0], date_from=self.date_from, date_to=self.date_to)
+            if (response.empty):
+                print("no price")
+                continue
+            date = get_date(response=response)
+            avg_price = get_avg_price(response, is_normalize=False)
+
+            print("====> back testing from "+date[-1][0]+" to "+date[0][0])
+            for i in range(0, len(response)):
+                end_date = date_str_to_datetime(date[i][0], include="ymd")
+                start_date = add_days(end_date, -self.days_interval)
+
+                ### the date list is in the reverse order
+                # print(response)
+
+                start_idx = self._get_idx_of_date(date, end_date)
+                end_idx = self._get_idx_of_date(date, start_date)
+
+                if(end_idx-start_idx <= 15):
+                    continue
+                record = self.detect_strategy.eval(
+                    response=response[start_idx:end_idx])
+                if record["detected"]:
+                    repeats += 1
+                    end_idx = self._get_idx_of_date(date, end_date)
+                    start_date = add_days(end_date, self.hold_priod)
+                    start_idx = self._get_idx_of_date(date, start_date)
+                    if(end_idx-start_idx <= 30):
+                        continue
+                    curr_price = avg_price[start_idx: end_idx]
+
+                    if self.price_explosed(curr_price[0], curr_price):
+                        success += 1
+
+        return success/repeats
+
+        #self.db.add_filter_record(symbol_id=symbol[0], record=record)
 
 
-
-
+# detect_strategy = Sample_BS(detect_strategy=detect_strategy,**bstconfigs )
--- modules/strategy/detect_strategy/candlestick_pattern_strategy.py
+++ modules/strategy/detect_strategy/candlestick_pattern_strategy.py
@@ -3,9 +3,51 @@ from .utils import *
 from myutils import *
 
 
-__all__ =["Candlestick_Pattern_Strategy"]
+__all__ = ["Candlestick_Pattern_Strategy"]
 
 
 class Candlestick_Pattern_Strategy(Detect_Strategy):
-    # To do
-    pass
+    def __init__(self, days_interval=120):
+        super().__init__()
+        self.days_interval = days_interval
+
+    def eval(self, response):
+        is_detected = False
+        response.index = response.time
+        date = get_date(response)
+        latest_date = date[0]
+        earliest_date = date[-1]
+
+        pattern, y = find_pattern(response, 'sym')
+        if pattern == None:
+            return
+        else:
+            is_detected = True
+
+        (x1, y1), (x2, y2) = pattern['line1']
+        (x3, y3), (x4, y4) = pattern['line2']
+        x1 = str(x1)[:10]
+        x2 = str(x2)[:10]
+        x3 = str(x3)[:10]
+        x4 = str(x4)[:10]
+
+        print("\n  +++++++++++++++")
+        print("  Candlestick Pattern is detected at " +
+              str(latest_date[0])[:10]+" since "+str(earliest_date[0])[:10])
+        print("  (x1,y1),(x2,y2): "+str((x1, y1))+str((x2, y2)))
+        print("  (x3,y3),(x4,y4): "+str((x3, y3))+str((x4, y4)))
+        print("  +++++++++++++++\n")
+
+        return {
+            "filter_name": "candlestick_pattern_strategy",
+            "detected": is_detected,
+            "begin_date": str(earliest_date[0])[:10],
+            "end_date": str(latest_date[0])[:10],
+            "details": {
+                "(x1,y1),(x2,y2)": str((x1, y1))+str((x2, y2)),
+                "(x3,y3),(x4,y4)": str((x3, y3))+str((x4, y4)),
+            },
+            "configs": {
+                "days_interval": str(self.days_interval)
+            },
+        }
